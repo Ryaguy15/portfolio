7bc4de7255cfed01b36f4c1e2ef45bc821c64f61
Ryan Philipps <raphilip@mtu.edu>
2020-02-27 14:28:39 -0500

    Added UIKit and started intro section

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'uikit';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h2>Hello, I'm <span className="primary-color">Ryan Philipps</span></h2>
+        <h1>Software Developer</h1>
+        <button className={"button-shadow"}>Resumé</button>
       </header>
     </div>
   );
